503eb1c8a4498374cd89bf4758ee553e12b8a6a5
Luigi MASTROSERIO <luigi.mastroserio@gmail.com>
2017-09-21 23:14:26 +0200

    add some query methods

--- src/main/java/org/kj6682/i/products/ProductRepository.java
+++ src/main/java/org/kj6682/i/products/ProductRepository.java
@@ -1,7 +1,13 @@
 package org.kj6682.i.products;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ProductRepository extends CrudRepository<Product, Long> {
 
+    List<Product> findByNameContainingIgnoreCase(@Param("name") String name);
+
+    List<Product> findByCategory(@Param("category") String category);
 }
