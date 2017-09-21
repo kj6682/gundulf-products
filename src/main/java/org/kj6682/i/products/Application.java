8154dbda2e9fb892d8e8baa80cebee5698c8743a
Luigi MASTROSERIO <luigi.mastroserio@gmail.com>
2017-09-22 00:46:30 +0200

    change ResouceUtils with reading via BufferedReader for reading files in a jar

--- src/main/java/org/kj6682/i/products/Application.java
+++ src/main/java/org/kj6682/i/products/Application.java
@@ -6,17 +6,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.util.Assert;
 import org.springframework.util.ResourceUtils;
 
+import javax.annotation.Resource;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class Application {
@@ -29,10 +34,18 @@ public class Application {
 	@Bean
 	CommandLineRunner initProducts(ProductRepository productRepository) throws IOException{
 
-		File jsonFile = ResourceUtils.getFile("classpath:products.json");
+	    /* It is not possible to use Spring ResourceUtils when reading resources files
+	       from a jar.
+	       So we should get back to basic read via BufferedReader and streams..
 
+	        https://stackoverflow.com/questions/5516020/bufferedreader-read-multiple-lines-into-a-single-string
+
+	        comment@Grzegorz Gajos
+	     */
+        org.springframework.core.io.Resource resource = new ClassPathResource("products.json");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(resource.getInputStream()));
+        final String jsonArray = reader.lines().collect(Collectors.joining());
 		ObjectMapper mapper = new ObjectMapper();
-		String jsonArray = new String(Files.readAllBytes(jsonFile.toPath()));
 
 		Product[] asArray = mapper.readValue(jsonArray, Product[].class);
 
