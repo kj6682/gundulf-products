0d4b5248d4dd79ee497b367fe17ded9e47b43496
Luigi MASTROSERIO <luigi.mastroserio@gmail.com>
2017-09-27 22:16:05 +0200

    add cors

--- src/main/java/org/kj6682/i/products/Application.java
+++ src/main/java/org/kj6682/i/products/Application.java
@@ -3,15 +3,22 @@ package org.kj6682.i.products;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -24,39 +31,64 @@ import java.util.stream.Collectors;
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
-	@Profile({"ddl-create", "h2"})
-	@Bean
-	CommandLineRunner initProducts(ProductRepository productRepository) throws IOException{
-
-	    /* It is not possible to use Spring ResourceUtils when reading resources files
-	       from a jar.
-	       So we should get back to basic read via BufferedReader and streams..
+    /*
+        It is not possible to use Spring ResourceUtils when reading resources files from a jar.
+	    So we should get back to basic read via BufferedReader and streams..
 
 	        https://stackoverflow.com/questions/5516020/bufferedreader-read-multiple-lines-into-a-single-string
 
-	        comment@Grzegorz Gajos
-	     */
+	    comment@Grzegorz Gajos
+	 */
+    @Profile({"ddl-create", "h2"})
+    @Bean
+    CommandLineRunner initProducts(ProductRepository productRepository) throws IOException {
         org.springframework.core.io.Resource resource = new ClassPathResource("products.json");
         BufferedReader reader = new BufferedReader(new InputStreamReader(resource.getInputStream()));
         final String jsonArray = reader.lines().collect(Collectors.joining());
-		ObjectMapper mapper = new ObjectMapper();
+        ObjectMapper mapper = new ObjectMapper();
 
-		Product[] asArray = mapper.readValue(jsonArray, Product[].class);
+        Product[] asArray = mapper.readValue(jsonArray, Product[].class);
 
-		return (evt) -> {
-			Arrays.asList(asArray).forEach(
-					cake -> {
-						productRepository.save(cake);
-						System.out.println(cake);
-					}
-			);
+        return (evt) -> {
+            Arrays.asList(asArray).forEach(
+                    cake -> {
+                        productRepository.save(cake);
+                        System.out.println(cake);
+                    }
+            );
 
-		};
+        };
 
-	}
+    }
+
+    @Configuration
+    public class GlobalRepositoryRestConfigurer extends RepositoryRestConfigurerAdapter {
+
+        @Value("${cors.pattern}")
+        private String CORS_BASE_PATTERN;
+
+        @Value("${cors.origin}")
+        private String ALLOWED_ORIGINS;
+
+        @Value("${cors.headers}")
+        private String ALLOWED_HEADERS;
+
+        @Value("${cors.methods}")
+        private String ALLOWED_METHODS;
+
+        @Override
+        public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+            config.getCorsRegistry()
+                    .addMapping(CORS_BASE_PATTERN)
+                    .allowedOrigins(ALLOWED_ORIGINS)
+                    .allowedHeaders(ALLOWED_HEADERS)
+                    .allowedMethods(ALLOWED_METHODS);
+        }
+
+    }
 
 }
--- src/main/java/org/kj6682/i/products/ProductRepository.java
+++ src/main/java/org/kj6682/i/products/ProductRepository.java
@@ -1,11 +1,12 @@
 package org.kj6682.i.products;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
-
 public interface ProductRepository extends PagingAndSortingRepository<Product, Long> {
 
     List<Product> findByNameContainingIgnoreCaseOrderByName(@Param("name") String name);
