98fa550ecf704cff00a1c94032947ba042bababf
Luigi MASTROSERIO <luigi.mastroserio@gmail.com>
2017-10-27 23:55:01 +0200

    start release 1.1.0

--- run_h2.sh
+++ run_h2.sh
@@ -2,7 +2,7 @@
 
 export PORT="-Dserver.port=9000"
 export SPRING_PROFILE="-Dspring.profiles.active=h2"
-export VERSION="0.0.1"
+export VERSION="1.1.0"
 
 mvn clean install -P h2
 java $PORT $JAVA_OPTS $SPRING_PROFILE -jar target/gundulf.products-$VERSION.jar
